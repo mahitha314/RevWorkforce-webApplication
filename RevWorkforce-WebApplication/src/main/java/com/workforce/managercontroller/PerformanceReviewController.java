835b4bc9b83d7e0b93d677e074d1375c32a2ce6c
prasanna <prasan3012003@gmail.com>
2026-02-21 18:53:33 +0530

    Added Manager Controller Package

--- RevWorkforce-WebApplication/src/main/java/com/workforce/managercontroller/PerformanceReviewController.java
+++ RevWorkforce-WebApplication/src/main/java/com/workforce/managercontroller/PerformanceReviewController.java
@@ -1,5 +1,29 @@
 package com.workforce.managercontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import com.workforce.managerservice.PerformanceReviewService;
+
+@Controller
+@RequestMapping("/manager/performance-reviews")
 public class PerformanceReviewController {
 
+	@Autowired
+	private PerformanceReviewService performanceReviewService;
+
+	@GetMapping
+	public String viewReviews(Model model) {
+		Long managerId = 1L;
+		model.addAttribute("reviews", performanceReviewService.getTeamReviews(managerId));
+		return "manager/performance_review";
+	}
+
+	@PostMapping("/submit")
+	public String submitReview(@RequestParam Long reviewId, @RequestParam int rating, @RequestParam String feedback) {
+		performanceReviewService.submitManagerReview(reviewId, rating, feedback);
+		return "redirect:/manager/performance-reviews";
+	}
 }
